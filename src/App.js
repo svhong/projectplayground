d6c1d7b632189503558868180b6910dbf36a2e19
Sean <writeseanhong@gmail.com>
2019-07-02 13:08:06 -0500

    starting with fresh copy of create react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ProductItem from "./components/ProductItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import productsData from "./components/productsData";
+
+const App = () => {
+
+    const newProductList = productsData.map(item => <ProductItem product={item} />);
+
+    return (
+        <div>
+            {newProductList}
+        </div>
+    )
 }
 
 export default App;
