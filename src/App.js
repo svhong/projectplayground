8dc55f864779f8f776779fc80e618a3f9b7a8524
Sean <writeseanhong@gmail.com>
2019-07-03 22:38:58 -0500

    removed whitespaces

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
-import ProductItem from "./components/ProductItem";
+// import ProductItem from "./components/ProductItem";
 
-import productsData from "./components/productsData";
+// import productsData from "./components/productsData";
 
-const App = () => {
+// const App = () => {
 
-    const newProductList = productsData.map(item => <ProductItem product={item} />);
+//     const newProductList = productsData.map(item => <ProductItem product={item} />);
 
-    return (
-        <div>
-            {newProductList}
+//     return (
+//         <div>
+//             {newProductList}
+//         </div>
+//     )
+// }
+
+class App extends React.Component {
+    render () {
+        return <div>
+            <h1>some stuff here</h1>
         </div>
-    )
+    }
 }
 
 export default App;
