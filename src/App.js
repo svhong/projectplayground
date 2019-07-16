349beea54788f1ede558e624886dd14357f29f46
Sean <writeseanhong@gmail.com>
2019-07-15 23:46:49 -0500

    created a form for a mock airline passenger form

--- src/App.js
+++ src/App.js
@@ -42,30 +42,159 @@ class App extends Component {
     //         </div>
     //     )
     // }
+    //***************************SWAPI************************ */
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         loading: false,
+    //         character: {}
+    //     }
+    // }
+
+    // componentDidMount() {
+    //     this.setState({ loading: true })
+    //     fetch("https://swapi.co/api/people/1")
+    //         .then(response => response.json())
+    //         .then(data => this.setState({
+    //             loading: false,
+    //             character: data
+    //         }))
+    // }
+
+    // render() {
+    //     const text = this.state.loading ? "loading..." : this.state.character.name
+    //     return (
+    //         <div>
+    //             {text}
+    //         </div>
+    //     )
+    // }
     constructor() {
         super();
         this.state = {
-            loading: false,
-            character: {}
-        }
+            firstName: "",
+            lastName: "",
+            age: "",
+            gender: "",
+            destination: "",
+            isVegan: false,
+            isGlutenFree: false,
+            isDairyFree: false
+
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    componentDidMount() {
-        this.setState({ loading: true })
-        fetch("https://swapi.co/api/people/1")
-            .then(response => response.json())
-            .then(data => this.setState({
-                loading: false,
-                character: data
-            }))
+    handleChange = (event) => {
+        const { name, value, type, checked } = event.target
+        type === "checkbox" ? this.setState({
+            [name]: checked
+        }) : this.setState({ [name]: value })
     }
 
+    handleClick = (event) => {
+        event.preventDefault();
+        console.log(this.state)
+    }
     render() {
-        const text = this.state.loading ? "loading..." : this.state.character.name
         return (
-            <div>
-                {text}
-            </div>
+            <main>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        name="firstName"
+                        value={this.state.firstName}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        name="lastName"
+                        value={this.state.lastName}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <input
+                        type="text"
+                        placeholder="Age"
+                        name="age"
+                        value={this.state.age}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <h3> Gender</h3>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="Male"
+                        checked={this.state.gender === "Male"}
+                        onChange={this.handleChange}
+                    />Male
+                    <br />
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="Female"
+                        checked={this.state.gender === "Female"}
+                        onChange={this.handleChange}
+                    />Female
+                    <br />
+                    <h3>Destination</h3>
+                    <select
+                        name="destination"
+                        onChange={this.handleChange}
+                        value={this.state.destination}
+                    >
+                        <option value="">--Please Select Destination--</option>
+                        <option value="Dubai">Dubai, United Arab Emirates</option>
+                        <option value="Auckland">Auckland, New Zealand</option>
+                        <option value="Tokyo">Tokyo, Japan</option>
+                    </select>
+                    <br />
+                    <h3>Diet Restrictions</h3>
+                    <input
+                        type="checkbox"
+                        name="isVegan"
+                        checked={this.state.isVegan}
+                        onChange={this.handleChange}
+                    />Vegan
+                    <br />
+                    <input
+                        type="checkbox"
+                        name="isGlutenFree"
+                        checked={this.state.isGlutenFree}
+                        onChange={this.handleChange}
+                    />Gluten Free
+                    <br />
+                    <input
+                        type="checkbox"
+                        name="isDairyFree"
+                        checked={this.state.isDairyFree}
+                        onChange={this.handleChange}
+                    />Dairy Free
+                    <br />
+                    <br />
+                    <button onClick={this.handleClick}>Submit</button>
+                </form>
+                <hr />
+                <h2>Entered information:</h2>
+                <p>
+                    Your name: {this.state.firstName} {this.state.lastName}
+                </p>
+                <p>Your age: {this.state.age}</p>
+                <p>Your gender: {this.state.gender}</p>
+                <p>Your destination: {this.state.destination}</p>
+                <p>
+                    Your dietary restrictions:
+                    {this.state.isVegan ? "Vegan " : " "}
+                    {this.state.isGlutenFree ? "Gluten Free " : " "}
+                    {this.state.isDairyFree ? "Dairy Free " : " "}
+
+                </p>
+            </main>
         )
     }
 }
